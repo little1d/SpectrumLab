ee9032498e2116a56b35e861aaaafe7e56970b1d
little1d <2431542525@qq.com>
2025-08-07 16:51:45 +0800

    Update zh config with favicon and theme changes
    Added a favicon link to the head section and updated themeConfig to use siteTitle and a new logo path. Also replaced the logo object with a string path and added siteTitle for improved site branding.

--- docs/.vitepress/zh.ts
+++ docs/.vitepress/zh.ts
@@ -5,10 +5,24 @@ export const zh = defineConfig({
     lang: 'zh-CN',
     title: "SpectrumLab",
     description: "A pioneering unified platform designed to systematize and accelerate deep learning research in spectroscopy.",
+
+    head: [
+        ['link', { rel: 'icon', type: 'image/svg+xml', href: './assets/logo.svg' }]
+        // ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+        // [
+        //     'link',
+        //     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
+        // ],
+        // [
+        //     'link',
+        //     { href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap', rel: 'stylesheet' }
+        // ]
+    ],
+
     themeConfig: {
-        logo: {
-            src: './assets/logo.svg',
-        },
+        siteTitle: "SpectrumLab",
+
+        logo: '/assets/logo.svg',
 
         nav: [
             { text: '教程', link: '/zh/tutorial' },
